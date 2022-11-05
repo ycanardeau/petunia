2eff5ee076d67b247bcb6d175521378535ad35bb
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-05 01:50:59 +0000

    WIP

--- Aigamo.Petunia/Program.cs
+++ Aigamo.Petunia/Program.cs
@@ -1,2 +1,23 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Aigamo.Petunia.Projects;
+
+namespace Aigamo.Petunia;
+
+internal class Program
+{
+	private static async Task Main(string[] args)
+	{
+		var projectPath = "petunia";
+		if (Directory.Exists(projectPath)) Directory.Delete(projectPath, recursive: true);
+		Directory.CreateDirectory(projectPath);
+
+		var project = new TypeScriptReactProject();
+		var projectFiles = project.GenerateProjectFiles();
+		await Task.WhenAll(projectFiles.Select(projectFile =>
+		{
+			var directoryName = Path.GetDirectoryName(projectFile.Path);
+			if (!string.IsNullOrEmpty(directoryName)) Directory.CreateDirectory(Path.Combine(projectPath, directoryName));
+
+			return File.WriteAllTextAsync(Path.Combine(projectPath, projectFile.Path), projectFile.Text);
+		}));
+	}
+}
