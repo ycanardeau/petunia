8759e685d3ed1514a179adcb3f5cb172c298443b
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-14 10:58:31 +0000

    Show modal

--- packages/petunia/src/App.tsx
+++ packages/petunia/src/App.tsx
@@ -1,7 +1,33 @@
+import {
+	EuiButton,
+	EuiModal,
+	EuiModalBody,
+	EuiModalFooter,
+	EuiModalHeader,
+	EuiModalHeaderTitle,
+	EuiProvider,
+} from '@elastic/eui';
+import '@elastic/eui/dist/eui_theme_dark.css';
 import React from 'react';
 
 const App = (): React.ReactElement => {
-	return <></>;
+	return (
+		<EuiProvider colorMode="dark">
+			<EuiModal onClose={(): void => {}}>
+				<EuiModalHeader>
+					<EuiModalHeaderTitle>
+						Create a new project{/* LOC */}
+					</EuiModalHeaderTitle>
+				</EuiModalHeader>
+
+				<EuiModalBody></EuiModalBody>
+
+				<EuiModalFooter>
+					<EuiButton fill>Create{/* LOC */}</EuiButton>
+				</EuiModalFooter>
+			</EuiModal>
+		</EuiProvider>
+	);
 };
 
 export default App;
