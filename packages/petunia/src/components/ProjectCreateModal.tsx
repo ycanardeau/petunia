c5ac22135111035117e277ffa9d575dc62ba6a7b
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-18 11:39:59 +0000

    Add link to GitHub

--- packages/petunia/src/components/ProjectCreateModal.tsx
+++ packages/petunia/src/components/ProjectCreateModal.tsx
@@ -363,6 +363,13 @@ export const ProjectCreateModal = observer((): React.ReactElement => {
 				>
 					Create{/* LOC */}
 				</EuiButton>
+				<EuiButton
+					size="s"
+					href="https://github.com/ycanardeau/petunia"
+					target="_blank"
+				>
+					GitHub
+				</EuiButton>
 			</EuiModalFooter>
 		</>
 	);
