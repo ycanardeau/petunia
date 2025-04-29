890ce09e2a1e24b38d86287fb7be3f146bdfe384
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-04-29 09:48:38 +0900

    Remove icons

--- src/features/typescript/projects/TypeScriptViteReactProject.ts
+++ src/features/typescript/projects/TypeScriptViteReactProject.ts
@@ -958,11 +958,11 @@ declare module '@elastic/eui/es/components/icon/*';
 import { ICON_TYPES } from '@elastic/eui';
 import { icon as alert } from '@elastic/eui/es/components/icon/assets/alert';
 import { icon as apps } from '@elastic/eui/es/components/icon/assets/apps';
-import { icon as arrowEnd } from '@elastic/eui/es/components/icon/assets/arrowEnd';
-import { icon as arrowStart } from '@elastic/eui/es/components/icon/assets/arrowStart';
 import { icon as arrowDown } from '@elastic/eui/es/components/icon/assets/arrow_down';
 import { icon as arrowLeft } from '@elastic/eui/es/components/icon/assets/arrow_left';
 import { icon as arrowRight } from '@elastic/eui/es/components/icon/assets/arrow_right';
+import { icon as arrowEnd } from '@elastic/eui/es/components/icon/assets/arrowEnd';
+import { icon as arrowStart } from '@elastic/eui/es/components/icon/assets/arrowStart';
 import { icon as calendar } from '@elastic/eui/es/components/icon/assets/calendar';
 import { icon as check } from '@elastic/eui/es/components/icon/assets/check';
 import { icon as copyClipboard } from '@elastic/eui/es/components/icon/assets/copy_clipboard';
@@ -970,8 +970,6 @@ import { icon as cross } from '@elastic/eui/es/components/icon/assets/cross';
 import { icon as dot } from '@elastic/eui/es/components/icon/assets/dot';
 import { icon as editorBold } from '@elastic/eui/es/components/icon/assets/editor_bold';
 import { icon as editorChecklist } from '@elastic/eui/es/components/icon/assets/editor_checklist';
-import { icon as editorCodeBlock } from '@elastic/eui/es/components/icon/assets/editor_code_block';
-import { icon as editorComment } from '@elastic/eui/es/components/icon/assets/editor_comment';
 import { icon as editorItalic } from '@elastic/eui/es/components/icon/assets/editor_italic';
 import { icon as editorLink } from '@elastic/eui/es/components/icon/assets/editor_link';
 import { icon as editorOrderedList } from '@elastic/eui/es/components/icon/assets/editor_ordered_list';
@@ -989,7 +987,6 @@ import { icon as returnKey } from '@elastic/eui/es/components/icon/assets/return
 import { icon as search } from '@elastic/eui/es/components/icon/assets/search';
 import { icon as sortDown } from '@elastic/eui/es/components/icon/assets/sort_down';
 import { icon as sortUp } from '@elastic/eui/es/components/icon/assets/sort_up';
-import { icon as userAvatar } from '@elastic/eui/es/components/icon/assets/userAvatar';
 import { icon as warning } from '@elastic/eui/es/components/icon/assets/warning';
 import { appendIconComponentCache } from '@elastic/eui/es/components/icon/icon';
 import { ValuesType } from 'utility-types';
@@ -1012,8 +1009,6 @@ const cachedIcons: IconComponentCacheType = {
 	dot,
 	editorBold,
 	editorChecklist,
-	editorComment,
-	editorCodeBlock,
 	editorItalic,
 	editorLink,
 	editorOrderedList,
@@ -1031,7 +1026,6 @@ const cachedIcons: IconComponentCacheType = {
 	search,
 	sortDown,
 	sortUp,
-	userAvatar,
 	warning,
 };
 
